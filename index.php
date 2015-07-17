506cc8a62fe8112e85fc428741935ded113c9e23
Lymdun <lymdun@protonmail.com>
2015-07-17 20:30:19 +0200

    Upload game

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link type="text/css" rel="stylesheet" href="./css/style.css" />
+        <title>Pac-Man</title>
+    </head>
+    <body>
+        <canvas width="361" height="418">Your browser does not support HTML5 technology.</canvas>
+        <script type="text/javascript" src="./js/jquery.min.js"></script>
+        <script type="text/javascript" src="./js/levels.js"></script>
+        <script type="text/javascript" src="./js/map.js"></script>
+        <script type="text/javascript" src="./js/food.js"></script>
+        <script type="text/javascript" src="./js/ghost.js"></script>
+        <script type="text/javascript" src="./js/player.js"></script>
+        <script type="text/javascript" src="./js/game.js"></script>
+        <div id="score">0</div>
+    </body>
+</html>
